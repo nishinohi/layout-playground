d58a4f83cb1c6a9fbbd01550531f6b7b3b77561d
azone-nishi <tomoya.nishi@azone-japan.co.jp>
2025-02-21 00:04:22 +0900

    [add] add logo animation

--- richsite/script.js
+++ richsite/script.js
@@ -1,1 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {})
+document.addEventListener('DOMContentLoaded', () => {
+  const logoSvg = document.getElementById('logo')
+  const logoSvgPath = document.querySelector('.cls-1')
+  // logoSvg の animate はあくまでアニメーションのトリガで
+  // アニメーションが設定されているのは path なのでパスにイベントを設定
+  logoSvgPath.addEventListener('animationend', () => {
+    logoSvg.classList.remove('animate')
+    setTimeout(() => logoSvg.classList.add('animate'), 5000)
+  })
+})
