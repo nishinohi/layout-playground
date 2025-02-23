2d93db33a292c45513b7352d420fe09761f9e563
azone-nishi <tomoya.nishi@azone-japan.co.jp>
2025-02-24 00:32:34 +0900

    [add] add slide in and out animation

--- view-transition/script.js
+++ view-transition/script.js
@@ -29,10 +29,23 @@ document.addEventListener('DOMContentLoaded', () => {
   grid.addEventListener('click', async (e) => {
     const item = e.target.closest('.grid-item')
     if (!item || item.classList.contains('active')) return
-    expandImage(item)
+
+    if (!document.startViewTransition) {
+      expandImage(item)
+      return
+    }
+    document.startViewTransition(async () => {
+      expandImage(item)
+    })
   })
 
   gridButton.addEventListener('click', async (e) => {
-    displayGrid()
+    if (!document.startViewTransition) {
+      displayGrid()
+      return
+    }
+    document.startViewTransition(() => {
+      displayGrid()
+    })
   })
 })
